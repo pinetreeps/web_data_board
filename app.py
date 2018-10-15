dad47b04a93f7ad0a3ae4abb7d5012e5f0652d93
pinetreeps <pangpangsongsong@sina.com>
2018-10-15 20:43:29 +0800

    debug about weather

--- app.py
+++ app.py
@@ -29,9 +29,15 @@ def hello_world():
 # 读设备取监控（散点图）数据
 device_monitor_data = utils.get_device_monitor_data(config.device_monitor_data)
 
+# 获取天气数据以及图片
+weather_data = utils.get_weather('beijing')
+weathre_img_path = utils.get_weather_img_path(weather_data[1])
+
 @app.route('/test2')
 def test2():
     return render_template('index02.html',
+                           weather_data=weather_data,
+                           weathre_img_path=weathre_img_path,
                            device_monitor_data=device_monitor_data,
                            department_name=config.department_name,
                            monitoring_coverage_rate=config.monitoring_coverage_rate,
@@ -43,6 +49,6 @@ def test2():
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8090, debug=True)
     # 线上
-    app.run(host='0.0.0.0', port=8097, debug=True)
+    # app.run(host='0.0.0.0', port=8097, debug=True)
     # 本机
-    # app.run(port=8090,debug=True)
+    app.run(port=8090,debug=True)
