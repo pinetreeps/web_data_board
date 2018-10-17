b2b053d303033974aae36c247efe7e6564740e5a
pinetreeps <pangpangsongsong@sina.com>
2018-10-17 20:27:42 +0800

    connect true data

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # python 3.6
 # Date: 2018/08/06
 # flask 展示地图
+import time
 from flask import Flask,request,make_response,render_template
 from flask_bootstrap import Bootstrap
 
@@ -26,18 +27,16 @@ def hello_world():
 # def test1():
 #     return render_template('index01.html')
 
-# 读设备取监控（散点图）数据
-device_monitor_data = utils.get_device_monitor_data(config.device_monitor_data)
-
-# 获取天气数据以及图片
-weather_data = utils.get_weather('beijing')
-weathre_img_path = utils.get_weather_img_path(weather_data[1])
-
-@app.route('/test2')
-def test2():
+@app.route('/test1')
+def test1():
+    # 读设备取监控（散点图）数据
+    device_monitor_data = utils.get_device_monitor_data(config.device_monitor_data)
+    # 获取天气数据以及图片
+    weather_data = utils.get_weather('beijing')
+    weather_img_path = utils.get_weather_img_path(weather_data[1])
     return render_template('index02.html',
                            weather_data=weather_data,
-                           weathre_img_path=weathre_img_path,
+                           weathre_img_path=weather_img_path,
                            device_monitor_data=device_monitor_data,
                            department_name=config.department_name,
                            monitoring_coverage_rate=config.monitoring_coverage_rate,
@@ -46,9 +45,38 @@ def test2():
                            shared_service_time_year=config.shared_service_time_year,
                            pie_data=config.pie_data)
 
+
+@app.route('/test2')
+def test2():
+    # 读取中心显示数据
+    center_data = utils.get_center_data()
+    # 读设备取监控（散点图）数据
+    device_monitor_data = utils.get_device_monitor_data(utils.get_api_data(config.API_URL_DEVICE_MONITOR, config.device_monitor_data))
+
+    # 读取bar1 监控覆盖率 vs 平均设备使用率
+    MCR_vs_ADUR = utils.deal_api_data_for_bar1()
+
+    # 读取bar2 年平均运行有效机时 vs 年平均共享服务机时
+    RETY_vs_SSTY = utils.deal_api_data_for_bar2()
+
+    # 获取天气数据以及图片
+    weather_data = utils.get_weather('beijing')
+    weather_img_path = utils.get_weather_img_path(weather_data[1])
+    return render_template('index02.html',
+                           center_data=center_data,
+                           weather_data=weather_data,
+                           weathre_img_path=weather_img_path,
+                           device_monitor_data=device_monitor_data,
+                           department_name1=MCR_vs_ADUR[0],
+                           monitoring_coverage_rate=MCR_vs_ADUR[1],
+                           avg_device_using_rate=MCR_vs_ADUR[2],
+                           department_name2=RETY_vs_SSTY[0],
+                           running_effective_time_year=RETY_vs_SSTY[1],
+                           shared_service_time_year=RETY_vs_SSTY[2],
+                           pie_data=config.pie_data)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8090, debug=True)
     # 线上
     # app.run(host='0.0.0.0', port=8097, debug=True)
     # 本机
-    # app.run(port=8090,debug=True)
+    app.run(port=8090,debug=True)
--- utils.py
+++ utils.py
@@ -6,17 +6,23 @@
 
 import requests
 import re
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
+import json
+
+import config
 
 
 def get_device_monitor_data(input_data):
+    # xx = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9',
+    #       'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9']
+    # yy = ['y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9',
+    #       'y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9',
+    #       'y0', 'y1', 'y2', 'y3']
+
     xx = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9',
-          'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9']
+          'x0', 'x1', 'x2', 'x3', 'x4']
     yy = ['y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9',
-          'y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9',
-          'y0', 'y1', 'y2', 'y3']
+          'y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7']
+
     device_running_list = []
     device_standby_list = []
     device_shutdown_list = []
@@ -34,14 +40,16 @@ def get_device_monitor_data(input_data):
             # print(input_data[num_device])
             # print(input_data[num_device].get('value'))
             device.append(
-                u'设备名称：' + input_data[num_device].get('name') + u'<br> 资产编号：' + input_data[num_device].get('device_code'))
-            if input_data[num_device].get('value') == "0":
+                # u'设备名称：' + input_data[num_device].get('name') + u'<br> 资产编号：' + input_data[num_device].get('device_code'))
+                u'设备名称：{dn} <br> 资产编号：{dc}'.format(dn=input_data[num_device].get('NAME'), dc=input_data[num_device].get('DEVICE_CODE')))
+            if input_data[num_device].get('VALUE') == "0":
                 device_running_list.append(device)
-            elif input_data[num_device].get('value') == "1":
+            elif input_data[num_device].get('VALUE') == "1":
                 device_standby_list.append(device)
-            elif input_data[num_device].get('value') == "2":
+            elif input_data[num_device].get('VALUE') == "2":
                 device_shutdown_list.append(device)
-            elif input_data[num_device].get('value') == "3":
+            # elif input_data[num_device].get('value') == "3":
+            elif input_data[num_device].get('VALUE') == "-1":
                 device_no_use_list.append(device)
             else:
                 print("do nothing")
@@ -81,14 +89,17 @@ def get_weather(city_str):
     #  使用正则抓取数据区块
     w_info = w_info_pattern.findall(whtml)
     # print(w_info)
-    temperature_pattern = re.compile("class=\"now\"><b>(.*?)</b>", re.S)
-    weather_data[0] = temperature_pattern.findall(w_info[0])[0]
+    try:
+        temperature_pattern = re.compile("class=\"now\"><b>(.*?)</b>", re.S)
+        weather_data[0] = temperature_pattern.findall(w_info[0])[0]
 
-    weather_pattern = re.compile("<span><b>(.*?)</b>", re.S)
-    weather_data[1] = weather_pattern.findall(w_info[0])[0]
+        weather_pattern = re.compile("<span><b>(.*?)</b>", re.S)
+        weather_data[1] = weather_pattern.findall(w_info[0])[0]
 
-    wind_pattern = re.compile("<b>风向：(.*?)</b>", re.S)
-    weather_data[2] = wind_pattern.findall(w_info[0])[0]
+        wind_pattern = re.compile("<b>风向：(.*?)</b>", re.S)
+        weather_data[2] = wind_pattern.findall(w_info[0])[0]
+    except Exception as e:
+        print("get weather data error, ", repr(e))
     return weather_data
 
 
@@ -108,10 +119,106 @@ def get_weather_img_path(weather_str):
     else:
         return 'static/images/leoweather/duoyun.png'
 
+def get_api_data(url, backup_data):
+    # 发送get请求
+    return_data = []
+    try:
+        # 获取返回的json数据转为字典
+        data = requests.get(url).json()
+        # print(data)
+        if data.get('message') == 'succes' and data.get('status') == '0':
+            return_data = data.get('result')
+        else:
+            print("api data message: {m}, status: {s}".format(m=data.get('message'), s=data.get('status')))
+            return_data = backup_data
+    except Exception as e:
+        print("get api data error,{}".format(repr(e)))
+        return_data = backup_data
+    return return_data
+
+def deal_api_data_for_bar1():
+    '''
+    为柱状图格式化数据1 监控覆盖率 vs 平均设备使用率
+    :return: 三个列表组成的列表
+    '''
+    return_lists = [[],[],[]]
+    monitoring_coverage_rate = get_api_data(config.API_URL_MONITORING_COVERAGE_RATE, config.BACKUP_DATA_MONITORING_COVERAGE_RATE)
+    avg_device_using_rate = get_api_data(config.API_URL_AVG_DEVICE_USING_RATE, config.BACKUP_DATA_AVG_DEVICE_USING_RATE)
+
+    for data1 in monitoring_coverage_rate:
+        return_lists[0].append(data1.get('NAME'))
+        return_lists[1].append(data1.get('VALUE'))
+        for data2 in avg_device_using_rate:
+            if data1.get('NAME') == data2.get('NAME'):
+                return_lists[2].append(data2.get('VALUE'))
+        if len(return_lists[1]) > len(return_lists[2]):
+            return_lists[2].append(0)
+    return return_lists
+
+def deal_api_data_for_bar2():
+    '''
+    为柱状图格式化数据2 年平均有效机时 vs 共享机时（暂时无数据）
+    :return:
+    '''
+    return_lists = [[],[],[]]
+    running_effective_time = get_api_data(config.API_URL_RUNNING_EFFECTIVE_TIME, config.BACKUP_DATA_RUNNING_EFFECTIVE_TIME)
+    shared_service_time = get_api_data(config.API_URL_SHARE_SERVICE_TIME, config.BACKUP_DATA_SHARE_SERVICE_TIME)
+    # print(running_effective_time)
+    # print(shared_service_time)
+    for data1 in running_effective_time:
+        return_lists[0].append(data1.get('NAME'))
+        return_lists[1].append(data1.get('VALUE'))
+        for data2 in shared_service_time:
+            if data1.get('NAME') == data2.get('NAME'):
+                return_lists[2].append(data2.get('VALUE'))
+        if len(return_lists[1]) > len(return_lists[2]):
+            return_lists[2].append(0)
+    return return_lists
+
+def get_center_data():
+    center_data = get_api_data(config.API_URL_CENTER_DISPLAY, config.BACKUP_DATA_CENTER_DISPLAY)
+    # print(center_data)
+    return_data = []
+    return_data.append(round(center_data[0].get('监控覆盖率'),1))
+    return_data.append(round(center_data[1].get('设备使用率'),1))
+    return return_data
+
 
 if __name__ == '__main__':
 
-    print(get_weather('beijing'))
+    print(get_center_data())
+    exit()
+
+    # 测试3
+    aa = get_api_data(config.API_URL_CENTER_DISPLAY, [])
+    print(type(aa))
+    print(aa)
+    exit()
+
+    bb = get_api_data(config.API_URL_AVG_DEVICE_USING_RATE,[])
+    print(len(bb))
+    for b in bb:
+        print(b)
+
+    cc = get_api_data(config.API_URL_MONITORING_COVERAGE_RATE, [])
+    print(len(cc))
+    for c in cc:
+        print(c)
+
+    dd = deal_api_data_for_bar1()
+    for d in range(len(dd[0])):
+        print(d, dd[0][d], dd[1][d], dd[2][d])
+
+    ee = deal_api_data_for_bar2()
+    for e in range(len(ee[0])):
+        print(e, ee[0][e], ee[1][e], ee[2][e])
+
+    exit()
+
+    ff = get_api_data(config.API_URL_DEVICE_MONITOR, [])
+    print(len(ff))
+    # for f in ff:
+    #     print(f,',')
 
     exit()
 
@@ -119,6 +226,15 @@ if __name__ == '__main__':
 
 
 
+    # 测试2
+    print(get_weather('beijing'))
+
+    exit()
+
+
+
+
+    # 测试1
     data = [[0,4,'设备名称：设备1 <br> 资产编号：ABC123'],[3,3,1],[2,6,1],[5,7,1],[0,8,1],[15,9,2]]
     data1 = [[0,0,'设备名称：设备1 <br> 资产编号：ABC123'],[0,1,'设备2'],[0,2,'设备3'],[0,23,'设备4']]
     data2 = [[1,1,'设备名称：设备1 <br> 资产编号：ABC123'],[3,2,20],[6,3,20]]
